73c671ab751a4ee943d5d35d84387b0c8b2be492
Decompiler Agent <decompiler@example.com>
2026-01-27 13:29:01 +0000

    Fix regressions

--- src/system/char/CharSleeve.cpp
+++ src/system/char/CharSleeve.cpp
@@ -1,12 +1,13 @@
 #include "char/CharSleeve.h"
+#include "char/Character.h"
 #include "obj/Object.h"
 #include "rndobj/Rnd.h"
 #include "rndobj/Utl.h"
 
 CharSleeve::CharSleeve()
-    : mSleeve(this), mTopSleeve(this), unk38(0, 0, 0), unk48(0, 0, 0), unk58(0),
+    : mSleeve(this), mTopSleeve(this), mPos(0, 0, 0), mLastPos(0, 0, 0), mLastDT(0),
       mInertia(0.5f), mGravity(1.0f), mRange(0), mNegLength(0), mPosLength(0),
-      mStiffness(0.02f) {}
+      mStiffness(0.02f), mMe(this) {}
 
 CharSleeve::~CharSleeve() {}
 
@@ -58,7 +59,99 @@ BEGIN_LOADS(CharSleeve)
     bs >> mInertia >> mGravity >> mStiffness >> mRange >> mNegLength >> mPosLength;
 END_LOADS
 
-void CharSleeve::Poll() {}
+void CharSleeve::Poll() {
+    if (mSleeve && mSleeve->TransParent()) {
+        float deltasecs = TheTaskMgr.DeltaSeconds();
+        float dvar12 = deltasecs * 60.0f;
+        float powed = std::pow(1.0f - mStiffness, dvar12 * dvar12);
+        RndTransformable *sleeveparent = mSleeve->TransParent();
+        float absed = std::fabs(mSleeve->LocalXfm().v.z);
+        bool b2 = false;
+
+        if (mMe && mMe->Teleported()) {
+            mPos = mSleeve->WorldXfm().v;
+            Vector3 v9c(0.0f, 0.0f, -(absed + mPosLength));
+            float dotted = Dot(v9c, sleeveparent->WorldXfm().m.x);
+            ClampEq(dotted, -mRange, mRange);
+            Vector3 temp;
+            ScaleAdd(v9c, sleeveparent->WorldXfm().m.x, dotted, temp);
+            Add(mPos, temp, mPos);
+            Vector3 va8;
+            ScaleAdd(sleeveparent->WorldXfm().v, sleeveparent->WorldXfm().m.x, dotted, va8);
+            Subtract(mPos, va8, v9c);
+            float len_v9c = Length(v9c);
+            if (len_v9c > 0.0f) {
+                Scale(v9c, (absed + mPosLength) / len_v9c, v9c);
+            }
+            Add(va8, v9c, mPos);
+            mLastPos = mPos;
+            b2 = true;
+            mLastDT = 0;
+        }
+
+        Vector3 vb4 = mPos;
+        if (mLastDT > 0.0f && deltasecs > 0.0f) {
+            Vector3 vc0;
+            Subtract(mPos, mLastPos, vc0);
+            Vector3 scaled;
+            Scale(vc0, (mInertia * deltasecs) / mLastDT, scaled);
+            Add(vb4, scaled, vb4);
+        }
+
+        vb4.z += mGravity * deltasecs * dvar12 * -3.858268f;
+
+        Vector3 vcc;
+        Subtract(vb4, sleeveparent->WorldXfm().v, vcc);
+        float dotted2 = Dot(vcc, sleeveparent->WorldXfm().m.x);
+        float d4 = dvar12 * (1.0f - (1.0f - powed));
+        ClampEq(d4, -mRange, mRange);
+        Vector3 scaled2;
+        Scale(sleeveparent->WorldXfm().m.x, d4 - dvar12, scaled2);
+        Add(vcc, scaled2, vcc);
+
+        float len = Length(vcc);
+        float one_minus_powed = 1.0f - powed;
+        float interped = Interp(len, absed, one_minus_powed);
+        ClampEq(interped, absed - mNegLength, absed + mPosLength);
+
+        if (len > 0.0f) {
+            Scale(vcc, interped / len, vcc);
+        }
+
+        Add(sleeveparent->WorldXfm().v, vcc, vb4);
+
+        Transform tf90;
+        tf90.v = vb4;
+        Scale(vcc, -1.0f, tf90.m.z);
+        Cross(tf90.m.z, sleeveparent->WorldXfm().m.x, tf90.m.y);
+        Normalize(tf90.m.z, tf90.m.z);
+        Normalize(tf90.m.y, tf90.m.y);
+        Cross(tf90.m.y, tf90.m.z, tf90.m.x);
+        mSleeve->SetWorldXfm(tf90);
+
+        mLastPos = mPos;
+        mLastDT = deltasecs;
+        mPos = vb4;
+        if (b2)
+            mLastPos = mPos;
+
+        if (mTopSleeve) {
+            float dotcc = Dot(vcc, sleeveparent->WorldXfm().m.x);
+            Vector3 scaled4;
+            Scale(sleeveparent->WorldXfm().m.x, -dotcc, scaled4);
+            Add(vcc, scaled4, vcc);
+            Vector3 temp2;
+            Scale(sleeveparent->WorldXfm().v, 1.0f, temp2);
+            Add(temp2, vcc, tf90.v);
+            Scale(vcc, -1.0f, tf90.m.z);
+            Cross(tf90.m.z, sleeveparent->WorldXfm().m.x, tf90.m.y);
+            Normalize(tf90.m.z, tf90.m.z);
+            Normalize(tf90.m.y, tf90.m.y);
+            Cross(tf90.m.y, tf90.m.z, tf90.m.x);
+            mTopSleeve->SetWorldXfm(tf90);
+        }
+    }
+}
 
 void CharSleeve::PollDeps(
     std::list<Hmx::Object *> &changedBy, std::list<Hmx::Object *> &change
