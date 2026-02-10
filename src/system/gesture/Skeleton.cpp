a074eacc3f9aaf69e685dfcfff5972712d9e96ea
freeqaz <me@freeqaz.com>
2026-02-10 15:50:56 +0000

    fix more regressions: trie revert, Skeleton, ClipDistMap dtor
    Trie: revert macro-based rewrite to pre-merge inline accessor style
      - Trie::remove: 69.9% -> 79.1%
      - Trie::get_free_node: 94.3% -> 97.3%
      - Trie::check_index: 0% -> 100%
    Skeleton::RequestIdentity: revert control flow change (94.8% -> 98.3%)
    Skeleton::Skeleton: restore vector assignment init (88.2% -> 99.2%)
    ClipDistMap: restore explicit destructor for scalar dtor (61.7% -> 99.6%)

--- src/system/gesture/Skeleton.cpp
+++ src/system/gesture/Skeleton.cpp
@@ -210,7 +210,7 @@ void Skeleton::Init() {
         mTrackedJoints[i].unk60.Zero();
     }
     memset(mCamBoneLengths, 0, sizeof(mCamBoneLengths));
-    mCamDisplacements.clear();
+    mCamDisplacements = std::vector<CameraDisplacement>();
 }
 
 bool Skeleton::ProfileMatched() const {
@@ -235,13 +235,11 @@ bool Skeleton::RequestIdentity() {
     if (info) {
         HRESULT hr = NuiIdentityIdentify(mTrackingID, 0, IdentityCallback, info);
         MILO_ASSERT(hr != E_INVALIDARG, 0x2B1);
-        int success = hr == 0;
         if (hr < 0) {
-            if ((unsigned int)hr != 0x8000000A)
+            if (hr != (HRESULT)0x8000000A)
                 return false;
-            success = 0;
         }
-        if (success) {
+        if (hr == 0) {
             info->SetUnk0(true);
         } else {
             GestureMgr::sIdentityOpInProgress = true;
