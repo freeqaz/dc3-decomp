f4a2787f529bb08894de0a184945c2f2c0f8b80e
freeqaz <me@freeqaz.com>
2026-03-16 08:13:38 +0000

    native: add PreloadSharedSubdirs diagnostic logging
    Logs subdirectory preload requests and array entries to help debug
    crowd clip loading and other asset dependency issues on native.

--- src/system/obj/Dir.cpp
+++ src/system/obj/Dir.cpp
@@ -616,6 +616,9 @@ void PreloadArray(DataArray *arr, int idx) {
                     shouldPop = true;
                 }
                 MILO_ASSERT(gPreloadIdx < DIM(gPreloaded), 0xA35);
+#ifdef HX_NATIVE
+                MILO_LOG("  PreloadArray[%d]: '%s'\n", gPreloadIdx, str);
+#endif
                 gPreloaded[gPreloadIdx++].LoadFile(str, false, true, kLoadFront, false);
                 if (shouldPop) {
                     MemPopHeap();
@@ -627,6 +630,10 @@ void PreloadArray(DataArray *arr, int idx) {
 
 void PreloadSharedSubdirs(Symbol s) {
     DataArray *arr = SystemConfig("preload_subdirs")->FindArray(s, false);
+#ifdef HX_NATIVE
+    MILO_LOG("PreloadSharedSubdirs('%s') arr=%p entries=%d\n",
+        s.Str(), arr, arr ? arr->Size() - 1 : 0);
+#endif
     if (arr) {
         PreloadArray(arr, 1);
     }
