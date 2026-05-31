fb72c03539f9b0642416917a018387974f8a8daa
freeqaz <me@freeqaz.com>
2026-05-31 11:58:43 +0000

    port synth_xbox XAPO effect family from og-dc3

--- src/system/synth_xbox/PitchShiftEffect.cpp
+++ src/system/synth_xbox/PitchShiftEffect.cpp
@@ -1,18 +1,11 @@
-// PitchShiftEffect.cpp - XAPO implementation for pitch shift effect
-// This file contains the template instantiation for the pitch shift effect processing unit
+#include "synth_xbox/PitchShiftEffect.h"
+#include "synth_xbox/soundtouch/source/SoundTouch/SoundTouch.h"
 
-#include "xdk/xapilibi/xbase.h"
-
-// Forward declarations for external functions used by the constructor
-extern "C" void XMemSet(void *ptr, int val, int size);
-
-// Template parameters
-struct PitchShiftEffectParams {
-    // Empty struct for parameters
-};
-
-// The XAPO base class template instantiation
-// This is a placeholder - the actual implementation will be linked from the binary
-namespace PitchShiftEffectImpl {
-    void Stub() {}
+PitchShiftEffect::PitchShiftEffect() : unk68(1), unk6c(2) {
+    mSoundTouch = new soundtouch::SoundTouch();
+    mSoundTouch->setSampleRate(48000);
+    mSoundTouch->setChannels(2);
+    mSoundTouch->setSetting(0, 1);
 }
+
+PitchShiftEffect::~PitchShiftEffect() { RELEASE(mSoundTouch); }
