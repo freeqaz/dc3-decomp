db359fe61c8bb3489b12ac7237d2ce257ead2463
freeqaz <me@freeqaz.com>
2026-05-31 05:59:15 +0000

    port system/synth/complex from og-dc3 (header companion for filterdesign)

--- src/system/synth/complex.cpp
+++ src/system/synth/complex.cpp
@@ -1,100 +1,54 @@
 #include "complex.h"
 #include <cmath>
 
-complex expj(double d1) {
-    struct complex result;
-    double dVar1 = sin(d1);
-    double dVar2 = cos(d1);
+// mkfilter -- given n, compute recurrence relation
+// to implement Butterworth, Bessel or Chebyshev filter of order n
+// A.J. Fisher, University of York   <fisher@minster.york.ac.uk>
+// September 1992
 
-    result.x = dVar2;
-    result.y = dVar1;
+static complex eval(complex[], int, complex);
+static double Xsqrt(double);
 
-    return result;
+/* evaluate response, substituting for z */
+complex evaluate(complex topco[], int nz, complex botco[], int np, complex z) {
+    return eval(topco, nz, z) / eval(botco, np, z);
 }
 
-complex csqrt(complex cplx) {
-    complex result;
-    double h;
-    double dy;
-    double dx;
-
-        auto _tmp0 = hypot(cplx.y, cplx.x);
-        dy = (h = _tmp0 - cplx.x) * 0.5;
-        if ((dy >= 0.0)) {
-        result.y = sqrt(dy);
-    } else {
-        result.y = 0.0;
-    }
-
-    dx = (cplx.y + h) * 0.5;
-    result.x = (dx >= 0.0) ? sqrt(dx) : 0.0;
-
-    if (cplx.y < 0.0)
-        result.y = -result.y;
-
-    return result;
+/* evaluate polynomial in z, substituting for z */
+static complex eval(complex coeffs[], int npz, complex z) {
+    complex sum = complex(0.0);
+    for (int i = npz; i >= 0; i--)
+        sum = (sum * z) + coeffs[i];
+    return sum;
 }
 
-complex cexp(complex cplx) {
-    struct complex result;
-    complex phase = expj(cplx.y);
-    double magnitude = exp(cplx.x);
-    phase.x = magnitude * phase.x;
-    phase.y = magnitude * phase.y;
-    result = phase;
-    return result;
+complex csqrt(complex x) {
+    double r = hypot(x);
+    complex z = complex(Xsqrt(0.5 * (r + x.re)), Xsqrt(0.5 * (r - x.re)));
+    if (x.im < 0.0)
+        z.im = -z.im;
+    return z;
 }
 
-complex operator/(complex cplx1, complex cplx2) {
-    complex result;
-    double dVar1 = 1.0 / (cplx2.x * cplx2.x + cplx2.y * cplx2.y);
-    result.x = (cplx1.y * cplx2.y + cplx1.x * cplx2.x) * dVar1;
-    result.y = (cplx1.y * cplx2.x - cplx1.x * cplx2.y) * dVar1;
-    return result;
+static double Xsqrt(double x) {
+    // because of deficiencies in hypot on Sparc, it's possible for
+    // arg of Xsqrt to be small and -ve, which logically it can't be
+    // (since r >= |x.re|). Take it as 0.
+    return (x >= 0.0) ? sqrt(x) : 0.0;
 }
 
-complex operator*(complex cplx1, complex cplx2) {
-    complex result;
-    result.x = cplx1.x * cplx2.x - cplx2.y * cplx1.y;
-    result.y = cplx2.x * cplx1.y + cplx2.y * cplx1.x;
-    return result;
+complex cexp(complex z) { return exp(z.re) * expj(z.im); }
+
+complex expj(double theta) { return complex(cos(theta), sin(theta)); }
+
+complex operator*(complex z1, complex z2) {
+    return complex(z1.re * z2.re - z1.im * z2.im, z1.re * z2.im + z1.im * z2.re);
 }
 
-// Evaluate polynomial using Horner's method.
-// coeff[0..degree] are the coefficients, z is the evaluation point.
-// Returns: coeff[0] + coeff[1]*z + ... + coeff[degree]*z^degree
-complex eval(complex * const coeff, int degree, complex z) {
-    complex result;
-    complex accum;
-    result.x = 0.0;
-    result.y = 0.0;
-    if (degree >= 0) {
-        int count = degree + 1;
-        complex *c = &coeff[degree];
-        do {
-            complex old = result;
-            complex cf = *c;
-            count--;
-            c--;
-            complex temp;
-            temp.y = old.x * z.y + old.y * z.x;
-            temp.x = z.x * old.x - z.y * old.y;
-            accum = temp;
-            accum.x += cf.x;
-            accum.y += cf.y;
-            result = accum;
-        } while (count != 0);
-    }
-    return result;
-}
-
-// Evaluate rational function: numerator(z) / denominator(z)
-complex evaluate(complex *const num, int numDeg, complex *const den, int denDeg, complex z) {
-    complex denom = eval(den, denDeg, z);
-    complex numer = eval(num, numDeg, z);
-    complex result;
-    double invMagSq = 1.0 / (denom.x * denom.x + denom.y * denom.y);
-    result.x = (numer.y * denom.y + numer.x * denom.x) * invMagSq;
-    result.y = (numer.y * denom.x - numer.x * denom.y) * invMagSq;
-    return result;
+complex operator/(complex z1, complex z2) {
+    double mag = (z2.re * z2.re) + (z2.im * z2.im);
+    return complex(
+        ((z1.re * z2.re) + (z1.im * z2.im)) / mag,
+        ((z1.im * z2.re) - (z1.re * z2.im)) / mag
+    );
 }
